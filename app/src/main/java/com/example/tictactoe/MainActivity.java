222285a7111014a6f4fb982974f4666fcafcfe77
menabaddo <edmundalexfresh@gmail.com>
2023-02-02 19:22:35 +0100

    Done with the front end and working on the backend implementation

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,10 +3,19 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private final List<int[]> combinationList = new ArrayList<>();
+
+    private int [] boxPositions =
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
 
         final LinearLayout playerOneLayout = findViewById(R.id.playerOneLayout);
         final LinearLayout playerTwoLayout = findViewById(R.id.playerTwoLayout);
+
+        final ImageView image1 = findViewById(R.id.image1);
+        final ImageView image2 = findViewById(R.id.image2);
+        final ImageView image3 = findViewById(R.id.image3);
+        final ImageView image4 = findViewById(R.id.image4);
+        final ImageView image5 = findViewById(R.id.image5);
+        final ImageView image6 = findViewById(R.id.image6);
+        final ImageView image7 = findViewById(R.id.image7);
+        final ImageView image8 = findViewById(R.id.image8);
+        final ImageView image9 = findViewById(R.id.image9);
+
 
     }
 }
