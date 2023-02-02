1a57da34236c6b607eb70de12632a75646b4fb81
menabaddo <edmundalexfresh@gmail.com>
2023-02-02 19:13:28 +0100

    Done with the front end and working on the backend implementation

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final TextView playerOneName = findViewById(R.id.player_one_name);
+        final  TextView playerTwoName = findViewById(R.id.player_two_name);
+
+
+        final LinearLayout playerOneLayout = findViewById(R.id.playerOneLayout);
+        final LinearLayout playerTwoLayout = findViewById(R.id.playerTwoLayout);
+
     }
 }
