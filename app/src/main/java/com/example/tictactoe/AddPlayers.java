42dd0f6ba2fd9978e8d65ec59ca42ad59e3ba573
menabaddo <edmundalexfresh@gmail.com>
2023-02-02 17:35:53 +0100

    Implemented a check on the player name edit text box so it won't take null values

--- app/src/main/java/com/example/tictactoe/AddPlayers.java
+++ app/src/main/java/com/example/tictactoe/AddPlayers.java
@@ -2,7 +2,12 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddPlayers extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class AddPlayers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_players);
+
+
+        final EditText playerOne = findViewById(R.id.player_one_name);
+        final EditText playerTwo = findViewById(R.id.player_two_name);
+        final Button startgameBtn = findViewById(R.id.startGameBtn);
+
+
+
+
+        startgameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                final String getPlayerOneName = playerOne.getText().toString();
+                final String getPlayerTwoName = playerTwo.getText().toString();
+
+
+                if(getPlayerOneName.isEmpty() || getPlayerTwoName.isEmpty()){
+                    Toast.makeText(AddPlayers.this, "Please enter player names", Toast.LENGTH_SHORT).show();
+
+
+                }else{
+                    Intent intent  = new Intent(AddPlayers.this, MainActivity.class);
+                    intent.putExtra("playerOne", getPlayerOneName);
+                    intent.putExtra("playerTwo", getPlayerTwoName);
+                    startActivity(intent);
+
+
+                }
+
+            }
+        });
+
+
     }
 }
