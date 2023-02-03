56df00001b9683a776710a6fe43abdcac46cf138
menabaddo <edmundalexfresh@gmail.com>
2023-02-03 12:57:19 +0100

    Implemented name display functionality on MainActivity.java.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -27,22 +28,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        final TextView playerOneName = findViewById(R.id.player_one_name);
-        final  TextView playerTwoName = findViewById(R.id.player_two_name);
+       playerOneName = findViewById(R.id.player_one_name);
+        playerTwoName = findViewById(R.id.player_two_name);
 
 
-        final LinearLayout playerOneLayout = findViewById(R.id.playerOneLayout);
-        final LinearLayout playerTwoLayout = findViewById(R.id.playerTwoLayout);
+        playerOneLayout = findViewById(R.id.playerOneLayout);
+         playerTwoLayout = findViewById(R.id.playerTwoLayout);
 
-        final ImageView image1 = findViewById(R.id.image1);
-        final ImageView image2 = findViewById(R.id.image2);
-        final ImageView image3 = findViewById(R.id.image3);
-        final ImageView image4 = findViewById(R.id.image4);
-        final ImageView image5 = findViewById(R.id.image5);
-        final ImageView image6 = findViewById(R.id.image6);
-        final ImageView image7 = findViewById(R.id.image7);
-        final ImageView image8 = findViewById(R.id.image8);
-        final ImageView image9 = findViewById(R.id.image9);
+        image1 = findViewById(R.id.image1);
+         image2 = findViewById(R.id.image2);
+         image3 = findViewById(R.id.image3);
+       image4 = findViewById(R.id.image4);
+       image5 = findViewById(R.id.image5);
+       image6 = findViewById(R.id.image6);
+       image7 = findViewById(R.id.image7);
+       image8 = findViewById(R.id.image8);
+       image9 = findViewById(R.id.image9);
+
+
+       combinationList.add(new int[]{0, 1, 2});
+        combinationList.add(new int[]{3, 4, 5});
+        combinationList.add(new int[]{6, 7, 8});
+        combinationList.add(new int[]{0, 3, 6});
+        combinationList.add(new int[]{1, 4, 7});
+        combinationList.add(new int[]{2, 5, 8});
+        combinationList.add(new int[]{2, 4, 6});
+        combinationList.add(new int[]{0, 4, 8});
+
+
+        final String getPlayerOneName = getIntent().getStringExtra("playerOne");
+        final String getPlayerTwoName = getIntent().getStringExtra("playerTwo");
+
+        playerOneName.setText(getPlayerOneName);
+        playerTwoName.setText(getPlayerTwoName);
+
+
+
+       playerOneLayout.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+           }
+       });
+
+        playerTwoLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
 
 
     }
